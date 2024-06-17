b93f4dcba86b85efed87203bf3b01b12b8b74faa
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-06-18 02:52:11 +0545

    added a network interface program and minor fixes to reachability program added package

---
+++ src/Lab/20240616/InterfaceListener.java
@@ -0,0 +1,14 @@
+package Lab._20240616;
+
+import java.net.*;
+import java.util.*;
+
+public class InterfaceListener {
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface ni = interfaces.nextElement();
+            System.out.println(ni);
+        }
+    }
+}
