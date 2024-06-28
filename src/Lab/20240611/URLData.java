565013ffa06add4e7016f31ae3aa50301ef8b462
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-06-28 14:16:55 +0545

    changed using logger class

--- src/Lab/20240611/URLData.java
+++ src/Lab/20240611/URLData.java
@@ -1,7 +1,9 @@
 package Lab._20240611;
+
 import java.net.*;
 import java.io.*;
-import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // Methods for retrieving data from URL
 // public InputStream openStream() throws IOException
@@ -10,23 +12,25 @@ import java.util.*;
 // public Object getContent() throws IOException
 // public Object getContent(Class[] classes) throws IOException
 @SuppressWarnings("deprecation")
-
 public class URLData {
+    private static final Logger logger = Logger.getLogger(URLData.class.getName());
+
     public static void main(String[] args) throws MalformedURLException {
-        // Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter URL: ");
-        // URL url = new URL(sc.nextLine());
-        // sc.close();
         try {
             URL url = new URL("https://likehome.neocities.org/");
+            logger.info(() -> "Fetching data from URL: " + url.toString());
 
             InputStream in = url.openStream();
             int c;
+            StringBuilder content = new StringBuilder();
+
             while ((c = in.read()) != -1) {
-                System.out.print((char) c);
+                content.append((char) c);
             }
+
+            logger.info(() -> "Content fetched from URL:\n" + content.toString());
         } catch (IOException ex) {
-            System.out.println(ex);
+            logger.log(Level.SEVERE, "An error occurred while fetching data from the URL", ex);
         }
     }
 }
